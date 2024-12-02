46e398367a683173685ce2a580ca1497297a5177
msalama <msalama@iskydev.com>
2024-12-02 19:16:25 +0200

    [ADD]

---
+++ execute_sql_and_code/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Sql Query or Code Execution",
+    'summary': """
+        This module allows users to execute SQL queries or Python code in Odoo.""",
+    'description': """
+        This module allows users to execute SQL queries or Python code in Odoo.
+    """,
+    'author': "Mahmoud Salama",
+    'website': "https://www.linkedin.com/in/mahmoud-salama-36a6a1164",
+    'category': 'Development Tools',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/sql_query_wizard_view.xml',
+    ],
+    'application': True,
+    'sequence': -100
+}
